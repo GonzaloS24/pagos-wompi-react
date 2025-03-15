4eb7e2d605a0c656b93460535630d9599db8f7b9
GonzaloS24 <gonzalo.salazar@chateapro.com>
2025-03-15 16:34:03 -0500

    production keys

--- src/api/wompiConfig.js
+++ src/api/wompiConfig.js
@@ -1,8 +1,8 @@
 export const WOMPI_CONFIG = {
-  PUBLIC_KEY: "pub_test_oRPBRklN6tpD4bJoRrkkd3X5l7kwzpF3",
-  INTEGRITY_SECRET: "test_integrity_RMUwn1SSLmR31OFgXOdgJcV2VkcaaDVL",
-  // PUBLIC_KEY: "pub_prod_mUzoGd0TQzkIWZwMamDL3ADjEYCO93N7",
-  // INTEGRITY_SECRET: "prod_integrity_KZkk9BdR7yGH9jDspvfhWud8IdUBnMQw",
+  // PUBLIC_KEY: "pub_test_oRPBRklN6tpD4bJoRrkkd3X5l7kwzpF3",
+  // INTEGRITY_SECRET: "test_integrity_RMUwn1SSLmR31OFgXOdgJcV2VkcaaDVL",
+  PUBLIC_KEY: "pub_prod_mUzoGd0TQzkIWZwMamDL3ADjEYCO93N7",
+  INTEGRITY_SECRET: "prod_integrity_KZkk9BdR7yGH9jDspvfhWud8IdUBnMQw",
   EXCHANGE_RATE_API: "https://api.exchangerate-api.com/v4/latest/USD",
   DEFAULT_WORKSPACE_ID: null,
 };
